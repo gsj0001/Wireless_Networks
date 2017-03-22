acd9414269b85980931464c473c039fb2b6e5a81
Andrew Peters <dpeters@myocv.com>
2017-03-22 18:33:28 -0500

    Added prompts to enter ip address and port number.

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -3,13 +3,42 @@ import java.net.*;
 
 public class UDPClient {
 	public static void main(String args[]) throws Exception {
-		BufferedReader inFromUser =
-		new BufferedReader(new InputStreamReader(System.in)); 
-		
+
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
 		DatagramSocket clientSocket = new DatagramSocket(); 
+
+		byte[] sendData = new byte[128];
+		byte[] emptyData = new byte[128];
+		byte[] receiveData = new byte[128];
+
+		InetAddress IPAddress = null; 
 		
-		InetAddress IPAddress = InetAddress.getByName("hostname"); 
-		
+		String hostAddress;
+		int serverPortNumber;
+		Scanner userInput = new Scanner(System.in);
+		while(true){		
+			System.out.print("Enter the server IP address: ");
+			hostAddress = userInput.nextLine();
+			try{
+				IPAddress = InetAddress.getByName(hostAddress);
+			} catch(UnknownHostException e){
+				System.out.println("Invalid host name or the host name is unreachable.");
+				continue;
+			}
+			break;
+		}
+
+		System.out.print("Enter the server port number: ");
+		serverPortNumber = userInput.nextInt();
+		String sentence;
+		String[] request;
+		do{
+			System.out.println("Type your HTTP Request and press Enter:");
+			sentence = inFromUser.readLine();
+			request = sentence.split("[ ]");
+		}while(request.length != 3);
+
+
 		byte[] sendData = new byte[1024];
 		byte[] receiveData = new byte[1024];
 		String sentence = inFromUser.readLine(); 
