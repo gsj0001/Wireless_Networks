eb99b331e0f49e690e16f705559887a6aaef62bd
Andrew Peters <dpeters@myocv.com>
2017-03-23 15:40:50 -0500

    final.

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -12,47 +14,56 @@ import java.net.InetAddress;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.util.Scanner;
+import java.security.MessageDigest;
 
 class UDPClient {
 	public static void main(String args[]) throws Exception {
 
-		//Reads in the keyboard stream by bytes and converts them into ASCII.
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		DatagramSocket clientSocket = new DatagramSocket();
 		InetAddress IPAddress = null;
 		byte[] sendData = new byte[128];
 		byte[] emptyData = new byte[128];
 		byte[] receiveData = new byte[128];
-		String serverIP;
-		int serverPortNumber;
+		String ip_address;
+		int portNum;
 		Scanner scan = new Scanner(System.in);
+
 		while(true){		
-			System.out.print("Enter the server IP address: ");
-			serverIP = scan.nextLine();
+			System.out.print("Enter the IP address of the server: ");
+			ip_address = scan.nextLine();
+
 			try{
-				IPAddress = InetAddress.getByName(serverIP);
+
+				IPAddress = InetAddress.getByName(ip_address);
+
 			} catch(UnknownHostException e){
-				System.out.println("Invalid host name or the host name is unreachable.");
+
+				System.out.println("Error: Invalid Host");
 				continue;
 			}
 			break;
 		}
 
 		System.out.print("Enter the server port number: ");
-		serverPortNumber = scan.nextInt();
+		portNum = scan.nextInt();
 		String sentence;
-		String[] request;
+		String[] userInput;
+
 		do{
-			System.out.println("Type your HTTP Request and press Enter:");
+			System.out.println("Enter the HTTP request: ");
 			sentence = inFromUser.readLine();
-			request = sentence.split("[ ]");
-		}while(request.length != 3);
+			userInput = sentence.split("[ ]");
 
-		File file = new File(System.getProperty("user.dir"), request[1]);
+		}while(userInput.length != 3);
+		
+		String filename = userInput[1];
+		String fileCheckSum = getMD5Checksum(filename);
 
-		//sending the data to the server.
+		File file = new File(System.getProperty("user.dir"), userInput[1]);
+
 		sendData = sentence.getBytes();
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, serverPortNumber);
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, portNum);
 		clientSocket.send(sendPacket); 
 		scan.close();
 
@@ -60,60 +71,108 @@ class UDPClient {
 
 		receivePacket = new DatagramPacket(receiveData, receiveData.length); 
 		clientSocket.setSoTimeout(2000);
+
 		try{
+
 		clientSocket.receive(receivePacket);
+
 		} catch(SocketTimeoutException e){
-			System.out.println("Too much time has gone by, connection is closed.");
+			System.out.println("Connection timeout.");
 			clientSocket.close();
 		}
+
 		if(!clientSocket.isClosed()){			
-		
 
-		String receivedPacketData = new String(receivePacket.getData());
-		String[] receivedPacketHeaderData = receivedPacketData.split("[ ]");
-		if(receivedPacketHeaderData.length > 3){
-			System.out.println(receivedPacketData);
+		String respnseFromServer = new String(receivePacket.getData());
+		String[] usrIn = respnseFromServer.split("[ ]");
+		if(usrIn.length > 3){
+			System.out.println(respnseFromServer);
 
-			//Receiving the header information
-			int indexOfcrlf=0;
-	
-			for(int i=0; i < 4; i++){
-				indexOfcrlf = receivedPacketData.indexOf('\n', indexOfcrlf+1);
+			int index = 0;
+			for(int i = 0; i < 4; i++){
+				index = respnseFromServer.indexOf('\n', index + 1);
 			}
-			String fileData = receivedPacketData.substring(indexOfcrlf+1);
-			int eos=-1;
-			while(eos == -1){
+
+			String fileData = respnseFromServer.substring(index + 1);
+			int x = -1;
+			while(x == -1){
 
 				receivePacket = new DatagramPacket(receiveData, receiveData.length); 
 				clientSocket.receive(receivePacket);
-				receivedPacketData = new String(receivePacket.getData());
-				System.out.println(receivedPacketData);
-				//clear the received data
+				respnseFromServer = new String(receivePacket.getData());
+				System.out.println(respnseFromServer);
+				
 				receiveData = emptyData.clone();				
-
-				//eos - end of stream, which indicates the first occurrence of an null byte 
-				eos = receivedPacketData.indexOf(0);
-				fileData = fileData + receivedPacketData;
+				
+				x = respnseFromServer.indexOf(0);
+				fileData = fileData + respnseFromServer;
 			}
 			clientSocket.close();
-			Writer writer = null;
+			
+			
+			
+			Writer fileWrite = null;
 			try {
 				fileData = fileData.trim();
-				writer = new BufferedWriter(new OutputStreamWriter(	new FileOutputStream(file), "UTF-8"));
-				writer.write(fileData);
-				writer.close();
+				fileWrite = new BufferedWriter(new OutputStreamWriter(	new FileOutputStream(file), "UTF-8"));
+				fileWrite.write(fileData);
+				fileWrite.close();
 
-			} catch (IOException ex) {
-			} finally {
+			} 
+			catch (IOException ex) {
+
+			} 
+			finally {
+
 				try {
-					writer.close();
-				} catch (Exception ex) {
+					fileWrite.close();
+				} 
+
+				catch (Exception ex) {
 				}
 			}
-		} else {
+		} 
+		else {
+
 			file =null;
-			System.out.println("Invalid response from Server.");
+			System.out.println("Error: bad server response.");
 		}
-	} 
+
+	    } 
+		
+	}
+	
+	public static byte[] createChecksum(String filename) throws
+    Exception
+	{
+	  InputStream fis =  new FileInputStream(filename);
+	
+	  byte[] buffer = new byte[1024];
+	  MessageDigest complete = MessageDigest.getInstance("MD5");
+	  int numRead;
+	  do {
+	   numRead = fis.read(buffer);
+	   if (numRead > 0) {
+	     complete.update(buffer, 0, numRead);
+	     }
+	   } while (numRead != -1);
+	  fis.close();
+	  return complete.digest();
+	}
+
+
+	public static String getMD5Checksum(String filename) throws Exception {
+	  byte[] b = createChecksum(filename);
+	  String result = "";
+	  for (int i=0; i < b.length; i++) {
+	    result +=
+	       Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+	   }
+	  return result;
+	}
+	
+	public static boolean checksumValidated(String checksumBefore, String checksumAfter)
+	{
+		return checksumBefore == checksumAfter;
 	}
 } 
