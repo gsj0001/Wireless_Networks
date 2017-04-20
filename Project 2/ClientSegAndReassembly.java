4a8fc9c4823c33fe6298562205861d2b74e2cf7e
Andrew Peters <dpeters@myocv.com>
2017-04-19 19:41:07 -0500

    Update.

--- Project 2/ClientSegAndReassembly.java
+++ Project 2/ClientSegAndReassembly.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class ClientSegAndReassembly {
 
 	ArrayList<Fragment> listOfFrags;
-	ErrorDetector ed = new ErrorDetector();
+	ErrorDetector detectError = new ErrorDetector();
 	byte[] reassembledData;
 	
 	
